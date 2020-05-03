59369643fdefaca9b9d9778aaaf0ab40fb58228f
Maheswaran <maheshwaran.gss@gmail.com>
2020-05-03 20:38:00 +0530

    Second commit for cards/deck.go

--- Cards/deck.go
+++ Cards/deck.go
@@ -45,7 +45,7 @@ func (d ceetu) saveToDrive(fileName string) error {
 func newDeckFromDrive(fileName string) (d ceetu) {
 	bs, err := ioutil.ReadFile(fileName)
 	if err != nil {
-		fmt.Println("Error:", err)
+		fmt.Println("Error1:", err)
 		os.Exit(1)
 
 	}
